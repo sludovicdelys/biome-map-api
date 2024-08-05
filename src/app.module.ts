61506ae09af81992b8321bc0ea4d1948a79fab4e
Sabrina Ludovic de Lys <ludovicdelyssabrina@gmail.com>
2024-08-05 12:37:25 +0200

    feat : implement map module and app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MapModule } from './map/map.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MapModule],
 })
 export class AppModule {}
